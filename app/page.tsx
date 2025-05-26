a562436df2e5013207a476b1525baecd374854c5
kennethdean <kennethjrdean99@gmail.com>
2025-05-26 21:41:00 +0800

    `Refactored code with minor formatting changes and removed unused import`

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Hero from "@/components/main/Hero";
 import Skills from "@/components/main/Skills";
 import Encryption from "@/components/main/Encryption";
@@ -6,13 +5,13 @@ import Projects from "@/components/main/Projects";
 
 export default function Home() {
   return (
-      <main className={'h-full w-full'} >
-        <div className={'flex flex-col  gap-20'}>
-            <Hero />
-            <Skills />
-            <Encryption />
-            <Projects />
-        </div>
-      </main>
+    <main className={"h-full w-full"}>
+      <div className={"flex flex-col  gap-20"}>
+        <Hero />
+        <Skills />
+        <Encryption />
+        <Projects />
+      </div>
+    </main>
   );
 }
